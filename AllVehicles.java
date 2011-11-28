1537402458955c3f3a96963a4e78c97a77d667b4
Ellen Engdahl <ellen.engdahl@gmail.com>
2011-11-28 15:06:51 +0100

    allVehicles side

---
+++ AllVehicles.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+
+import javax.swing.*;
+
+
+public class AllVehicles {
+	
+	private JFrame frame;
+	private Container contentPane;
+	
+	public AllVehicles() 
+	{
+		makeFrame();
+	}
+
+	private void makeFrame() 
+	{
+		
+		frame = new JFrame();
+		contentPane = frame.getContentPane();
+		
+		//make the panel for the allVehicles tab
+		JPanel allVehiclesPanel = new JPanel();
+		allVehiclesPanel.setLayout(new BorderLayout());
+		contentPane.add(allVehiclesPanel);
+		
+		
+		JPanel tablePanel = new JPanel();
+		allVehiclesPanel.add(tablePanel, BorderLayout.CENTER);
+	
+		JPanel searchPanel = new JPanel();
+		FlowLayout searchLayout = new FlowLayout();
+		searchLayout.setAlignment(0);
+		searchPanel.setLayout(searchLayout);
+		allVehiclesPanel.add(searchPanel, BorderLayout.NORTH);
+	
+		JLabel licenseLabel = new JLabel("License nr.");
+		searchPanel.add(licenseLabel);
+		
+		JTextField licenceText = new JTextField(20);
+		searchPanel.add(licenceText);
+		
+		JButton searchButton = new JButton("Search");
+		searchPanel.add(searchButton);
+		
+		// Display
+		frame.pack();
+		frame.setVisible(true);
+	
+	}
+}
---
+++ Frame.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Frame {
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//Reservation reservation = new Reservation();
+		AllVehicles vehiclespanel = new AllVehicles();
+		
+	}
+	
+	
+
+}
