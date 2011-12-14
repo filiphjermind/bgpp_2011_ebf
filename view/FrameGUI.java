9da2cddedc058c14dea0a2eedf5490d97bcd8ded
Filip Hjermind <filiphjermind@gmail.com>
2011-12-14 16:46:24 +0100

    adding code to close dbconnection when window closes

--- view/FrameGUI.java
+++ view/FrameGUI.java
@@ -115,44 +115,31 @@ public class FrameGUI {
 			
 			@Override
 			public void windowOpened(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
 			}
 			
 			@Override
 			public void windowIconified(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
 			}
 			
 			@Override
 			public void windowDeiconified(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
 			}
 			
 			@Override
 			public void windowDeactivated(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
 			}
 			
 			@Override
 			public void windowClosing(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				quit();
 			}
 			
 			@Override
 			public void windowClosed(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
 			}
 			
 			@Override
 			public void windowActivated(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
 			}
 		});
 	}
